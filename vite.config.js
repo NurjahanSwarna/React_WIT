c43e765810caf7c5288b4b4da89c4708cb293671
Nurjahan Swarna <nurjahanswarna999@gmail.com>
2025-02-01 02:34:26 +0700

    Updated files

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["slick-carousel"]
-    }
-  }
-});
+
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ["slick-carousel"]
+//     }
+//   }
+// });
 // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
 // }),
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {  
+//     rollupOptions: {
+//       external: ['react', 'react-dom', 'react-redux']
+//     }
+//   }
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
+});
+
 
