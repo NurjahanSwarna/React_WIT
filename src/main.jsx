2050a91ad5f078012cd39ceb7923f0707781e5a7
Nurjahan Swarna <nurjahanswarna999@gmail.com>
2025-02-01 01:26:01 +0700

    Updated files

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ContextApi } from './Components/ContextApi.jsx'
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
 import { Provider } from 'react-redux'
 import Store from './Store.js';
 import firebaseConfig from './Components/firebase.config.js'
