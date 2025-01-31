6104a883f24dc4b579c66fb7cc2464f33db605ea
Nurjahan Swarna <nurjahanswarna999@gmail.com>
2025-02-01 02:58:35 +0700

    Updated

--- src/Store.js
+++ src/Store.js
@@ -1,5 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit'
+// import { configureStore } from '@reduxjs/toolkit'
 import { CurtSlice } from './Components/Slices/CurtSlice'
+import { configureStore } from "@reduxjs/toolkit";
+
 export default configureStore({
   reducer: {
     CurtItemManager : CurtSlice.reducer,
--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,15 @@
 
-
-
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     rollupOptions: {
-//       external: ["slick-carousel"]
-//     }
-//   }
-// });
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// }),
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {  
-//     rollupOptions: {
-//       external: ['react', 'react-dom', 'react-redux']
-//     }
-//   }
-// })
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
-  }
+    rollupOptions: {
+      external: ['react-redux'],
+    },
+  },
 });
+
 
 
